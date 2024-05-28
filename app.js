7aaa64dea8ce780254eb86ed1cff7665cc9e93cb
Reema Thakur <reemathakur2114@gmail.com>
2024-05-28 20:57:14 +0530

    sharing data across requests & users

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-
 const app = express();
-
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public"))); // This allows us to get public css file in shop.html
 
-app.use("/admin", adminRoutes); // filtering segment
+app.use("/admin", adminData.routes); // filtering segment
 app.use(shopRoutes);
 
 // for 404 request
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,11 @@
 const path = require("path");
-
 const express = require("express");
-
 const rootDir = require("../util/path");
-
 const router = express.Router();
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
+  console.log(adminData.products); // This way we can get the data, but the problem is this data is inehrent to our node server as it is running therefore shared across all users (if we add book from chrome & open localhost in firefox I still get that book in products there)
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
